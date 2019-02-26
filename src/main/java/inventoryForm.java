a95e75cc02b228b337c814301b046efe8774eda8
Dylan Chesnouard <dylan.chesnouard@protonmail.com>
2019-02-26 01:52:51 +0100

    Ajout de la liste de materiel dynamique (need SQL)

--- src/main/java/inventoryForm.java
+++ src/main/java/inventoryForm.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class inventoryForm {
     private JPanel inventoryPanel;
@@ -11,14 +14,12 @@ public class inventoryForm {
     private JLabel lbl_nom;
     private JLabel lbl_prenom;
     private JLabel lbl_role;
-    private JScrollBar scrollBar1;
-    private JCheckBox checkBox1;
-    private JCheckBox checkBox2;
-    private JCheckBox checkBox3;
     private JLabel image;
+    private JPanel materialPanel;
 
     private static JFrame frame;
     private static String userMatricule;
+    private static JPanel invPanel;
 
     public inventoryForm() {
 
@@ -33,6 +34,42 @@ public class inventoryForm {
         ImageIcon scaledImage = new ImageIcon(originalImage.getImage().getScaledInstance(200,150, Image.SCALE_SMOOTH));
         image.setIcon(scaledImage);
 
+        // On récupère la liste des Materiel de la BDD
+        ArrayList<Materiel> materiels = Materiel.getListMateriel();
+
+        // On ajoute la liste
+        materialPanel.setLayout(new BoxLayout(materialPanel, BoxLayout.PAGE_AXIS));
+        for (Materiel materiel : materiels) {
+            //boolean isEmpty = Materiel.isEmpty(materiel.getNom());
+            boolean isEmpty = false;
+
+            JPanel matPan = new JPanel();
+            matPan.setLayout(new BorderLayout());
+
+            // ----- LABEL
+            JLabel lbl = new JLabel(materiel.getNom());
+            matPan.add(lbl, BorderLayout.LINE_START);
+            lbl.setEnabled(!isEmpty);
+
+            // ----- CHECKBOX
+            Checkbox cb = new Checkbox();
+            cb.setEnabled(!isEmpty);
+            cb.addItemListener(new ItemListener() {
+                @Override
+                public void itemStateChanged(ItemEvent e) {
+                    if (cb.getState()) {
+                        System.out.println("SELECTED " + materiel.getNom() + " !");
+                    } else {
+                        System.out.println("UNSELECTED " + materiel.getNom() + " !");
+                    }
+                }
+            });
+            matPan.add(cb, BorderLayout.LINE_END);
+            matPan.setMaximumSize(new Dimension(Integer.MAX_VALUE, matPan.getMinimumSize().height + 5));
+
+            materialPanel.add(matPan);
+        }
+
         bt_back.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -45,7 +82,8 @@ public class inventoryForm {
     public static void main(String[] args, String matricule) throws MalformedURLException {
         userMatricule = matricule;
         frame = new JFrame("inventoryForm");
-        frame.setContentPane(new inventoryForm().inventoryPanel);
+        invPanel = new inventoryForm().inventoryPanel;
+        frame.setContentPane(invPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.pack();
